10d07ca23d6d2a1ff3e730b13ca3f26b8c176d82
mayzinei <mze1001web@gmail.com>
2024-02-15 21:56:25 +0630

    create post method

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
 const express = require("express");
 const app = express();
-const PORT = 4000;
+const nodemailer = require("nodemailer");
+const parser = require("body-parser");
+const cors = require("cors");
+require("dotenv/config");
+const PORT = process.env.PORT || 4001;
 
+const transporter = nodemailer.createTransport({
+	service: "Gmail",
+	host: "smtp.gmail.com",
+	port: 587,
+	secure: false,
+	auth: {
+		user: process.env.EMAIL_USER,
+		pass: process.env.EMAIL_PASS,
+	},
+});
+
+app.use(parser.json());
+app.use(cors());
 app.get("/", (req, res) => {
 	return res.json("Hello World");
 });
 
-app.listen(PORT, (req, res) => {
+app.post("/contact", (req, res) => {
+	const { name, phone, email } = req.body;
+	const data = {
+		name,
+		phone,
+		email,
+	};
+	return res.json(data);
+});
+
+app.listen(PORT, () => {
 	console.log(`Server is running at port : ${PORT}`);
 });
