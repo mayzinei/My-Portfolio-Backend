00e3788d8ee17b630324617520f9da67ccd5bcc5
mayzinei <mze1001web@gmail.com>
2024-03-18 20:21:08 +0630

    fix reply message

--- app.js
+++ app.js
@@ -34,10 +34,12 @@ app.post("/contact", async (req, res) => {
 	const { name, phone, email, subject, message } = req.body;
 
 	const mailOptions = {
-		from: process.env.EMAIL_USER, //send from
-		to: email, ///to someone
-		subject: `Hello ${name}. I received well your ${subject} . I will connect soon ${phone}`, //subnt
-		text: message, //html
+		from: process.env.EMAIL_USER,
+		to: email,
+		// subject : subject,
+		text: `Dear ${name}.
+			I received well your email : ${subject}. I will contact you soon.
+			Thank you & best regards.`,
 	};
 
 	transporter.sendMail(mailOptions, async (error, info) => {
